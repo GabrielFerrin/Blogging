cc0368b65ae524d96f8c142ef737db51e1763f47
GabrielFerrin <gabrielferrin@byui.edu>
2024-05-18 17:16:52 -0500

    deply tray

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,10 +4,10 @@ import {
   getUserByID,
   getUserByUsername,
   getUserByEmail
-} from '../controllers/user/user-GET.controller.js'
-import { addUser } from '../controllers/user/user-POST.controller.js'
-import { deleteUser } from '../controllers/user/user-DELETE.controller.js'
-import { updateUser } from '../controllers/user/user-PATCH.controller.js'
+} from '../controllers/user/user-get.controller.js'
+import { addUser } from '../controllers/user/user-post.controller.js'
+import { deleteUser } from '../controllers/user/user-delete.controller.js'
+import { updateUser } from '../controllers/user/user-patch.controller.js'
 
 const router = Router()
 
