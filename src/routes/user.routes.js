7842adec547a6ad804e109ead1d75189fbf4c7bd
GabrielFerrin <gabrielferrin@byui.edu>
2024-05-19 00:40:49 -0500

    try with .controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,9 @@
 import Router from 'express'
+import { getUsers } from '../controllers/user/user.controller.js'
 
 const router = Router()
 
 // GET
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+router.get('/', getUsers)
 
 export default router
