9774c21ca5c101f3d5dca7510b421ebd6657090a
GabrielFerrin <gabrielferrin@byui.edu>
2024-05-19 20:49:13 -0500

    error fix 2

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,17 @@
 import Router from 'express'
-import { getUsers } from '../controllers/user/user-GET.controller.js'
+import {
+  getUsers,
+  getUserByID,
+  getUserByEmail,
+  getUserByUsername
+} from '../controllers/user/user-GET.controller.js'
 
 const router = Router()
 
 // GET
 router.get('/', getUsers)
+router.get('/id/:id', getUserByID)
+router.get('/email/:email', getUserByEmail)
+router.get('/username/:username', getUserByUsername)
 
 export default router
