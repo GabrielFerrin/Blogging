6f7e57c85cc78176aa7dc7aae1436d3d26cb5c58
GabrielFerrin <gabrielferrin@byui.edu>
2024-05-19 20:46:25 -0500

    error fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,26 +1,9 @@
 import Router from 'express'
-import {
-  getUsers,
-  getUserByID,
-  getUserByUsername,
-  getUserByEmail
-} from '../controllers/user/user-GET.controller.js'
-import { addUser } from '../controllers/user/user-post.controller.js'
-import { deleteUser } from '../controllers/user/user-DELETE.controller.js'
-import { updateUser } from '../controllers/user/user-PATCH.controller.js'
+import { getUsers } from '../controllers/user/user-GET.controller.js'
 
 const router = Router()
 
 // GET
 router.get('/', getUsers)
-router.get('/id/:id', getUserByID)
-router.get('/username/:username', getUserByUsername)
-router.get('/email/:email', getUserByEmail)
-// POST
-router.post('/', addUser)
-// PATCH
-router.patch('/:id', updateUser)
-// DELETE
-router.delete('/:id', deleteUser)
 
 export default router
